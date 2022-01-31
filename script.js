928c5f686592ce8e8289b92cc6635795724c37c1
mobak88 <mostafa0401@ymail.com>
2022-01-31 12:58:41 +0100

    Adding feed animal for dog

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-class Animal {
-  constructor(species, animalName, country, color) {
-    this.species = species;
-    this.animalName = animalName;
-    this.country = country;
-    this.color = color;
-  }
+const animalSpecies = document.querySelector('.animal-species');
+const animalColor = document.querySelector('.animal-color');
+const animalName = document.querySelector('.animal-name');
+const animalCountry = document.querySelector('.animal-country');
+const animalImage = document.querySelector('.animal-image');
 
-  feedAnimal() {
-    console.log(`${this.animalName} have been fed`);
-  }
+class Animal {
+    // Constructor creates new object
+    // Parameter defines what the new object will contain
+    constructor (species, animalName, country, color, img) {
+        this.species = species;
+        this.animalName = animalName;
+        this.country = country;
+        this.color = color;
+        this.img = img;
+    }
+
+    // Class method to be used with objects
+    feedAnimal() {
+        console.log(`${this.animalName} have been fed`);
+    }
 }
 
-const Dog = new Animal('Dog', 'Tom', 'norway', 'Black');
+// New object instance of animal
+const Dog = new Animal('Dog', 'Tom', 'norway', 'Black', './images.jpg');
+
 console.log(Dog.species);
+console.log(Dog);
+
+animalSpecies.innerHTML = Dog.species;
+animalColor.innerHTML = Dog.color;
+animalName.innerHTML = Dog.animalName;
+animalCountry.innerHTML = Dog.color;
+animalImage.innerHTML += `<img src='${Dog.img}' />`;
+
+Dog.feedAnimal();
